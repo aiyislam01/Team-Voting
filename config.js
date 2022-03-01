92ef5101956ecc5d41cf6707e9922dc8b4ac4c3e
aiyislam01 <aiyaanaicmcl@gmail.com>
2022-02-28 19:16:28 -0800

    test

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+    const firebaseConfig = {
+        apiKey: "AIzaSyDsr08W0GafIbC3LpHobV_lTwJqlOwrH94",
+        authDomain: "team-voting-ec139.firebaseapp.com",
+        databaseURL: "https://team-voting-ec139-default-rtdb.firebaseio.com",
+        projectId: "team-voting-ec139",
+        storageBucket: "team-voting-ec139.appspot.com",
+        messagingSenderId: "592483772959",
+        appId: "1:592483772959:web:036563166d04894932432a"
+      };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
